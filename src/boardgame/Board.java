b35659f8d9d5064702bda44c7834a39a7b1d02ee
Matheus Rafael <matheusrafael2005@gmail.com>
2025-05-29 14:42:06 -0300

    moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,7 @@
 package boardgame;
 
+import chess.ChessException;
+
 public class Board {
 	
 	public Board(int rows, int columns) {
@@ -41,6 +43,19 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position=null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
@@ -55,4 +70,6 @@ public class Board {
 		}	
 		return piece(position) != null;
 	}
+	
+
 }
